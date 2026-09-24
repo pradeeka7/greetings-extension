b18b0a343f871e63f6ae43ecce2367967aead12b
Pradeeka Seneviratne <36432168+pradeeka7@users.noreply.github.com>
2026-09-24 18:12:56 +0200

    add annotation

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
+/**
+ * Greeting blocks
+ */
+//% color="#AA278D" weight=100
 namespace greetings {
+
+    /**
+     * Displays Hello on the micro:bit.
+     */
+    //% block="show hello"
     export function showHello(): void {
         basic.showString("Hello")
     }
